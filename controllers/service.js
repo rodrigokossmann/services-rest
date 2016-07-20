4db348faf1dbba7f91f8741b02bb4c34b1978d29
RODRIGO KOSSMANN <rodrigo.kossmann@gmail.com>
2016-07-19 23:22:21 -0300

    Correcao

--- controllers/service.js
+++ controllers/service.js
@@ -1,13 +1,13 @@
 "use strict";
 
-	var express = require('express')
-  	, router = express.Router()
-  	, Service = require('../models/service')
+var express = require('express')
+, router = express.Router()
+, Service = require('../models/service')
 
-	router.get('/find/:id', function(req, res){
-	   	Service.get(req.params.id, function(service) {
-	   		res.send(service);
-	   	});
-	 });
+router.get('/find/:id', function(req, res){
+   	Service.get(req.params.id, function(service) {
+   		res.send(service);
+   	});
+});
 
-	module.exports = router;
+module.exports = router;
